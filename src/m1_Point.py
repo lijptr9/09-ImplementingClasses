ed6d2a918a2a53145bafbab5ee4d8c067f5f6cae
lijptr9 <lij8@rose-hulman.edu>
2018-03-22 08:34:34 -0400

    done

--- src/m1_Point.py
+++ src/m1_Point.py
@@ -49,6 +49,10 @@ class Point(object):
         self.x= x
         self.y= y
 
+
+    def __repr__(self):
+        return 'Point({}, {})'.format(self.x, self.y)
+
 def run_test_init():
     """
     Tests the   __init__   method of the Point class.
@@ -96,7 +100,7 @@ def run_test_init():
         print('Actual for p2:  ', p2.x, p2.y)
     """
     # ------------------------------------------------------------------
-    # TODO: 3.
+    # DONE: 3.
     #   a. Read the above specification of the   __init__   method.
     #        Do NOT proceed until you understand WHAT it should do
     #        (but not necessarily HOW it will do it).
@@ -194,6 +198,25 @@ def run_test_repr():
     print('-----------------------------------------------------------')
     print('Testing the   __repr__   method of the Point class.')
     print('-----------------------------------------------------------')
+
+    p1 = Point(30, 18)
+    print()
+    print('Expected for p1: Point(30, 18)')
+    print('Actual for p1:  ', p1)
+
+    p2 = Point(100, -40)
+    print()
+    print('Expected for p2: Point(100, -40)')
+    print('Actual for p2:  ', p2)
+    print('Expected for p1: Point(30, 18)')
+    print('Actual for p1:  ', p1)
+
+    p1.y = 999
+    print()
+    print('Expected for p1: Point(30, 999)')
+    print('Actual for p1:  ', p1)
+    print('Expected for p2: Point(100, -40)')
+    print('Actual for p2:  ', p2)
 
 
 def run_test_clone():
